221ee6f21ccffc7aef90e148f2bdf3fa6f839be1
Arnau Gausachs <arnau.gausachs.gancedo@gmail.com>
2026-02-24 16:06:55 +0100

    creo que ya esta, comienzo la prueba

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'home',
   imports: [],
-  templateUrl: '/home.html',
-  styleUrl: 'home.css',
+  templateUrl: './home.html',
+  styleUrl: './home.css',
 })
 export class HomeComponent {
 
