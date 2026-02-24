bf54f2d7817cf47c54dab573d66553b5c3ccf009
Olalla <olallasanchezbernad08@gmail.com>
2026-02-24 16:25:06 +0100

    tratamientos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { HomeComponent } from './home/home';
+import { Tratamientos } from './tratamientos/tratamientos';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'tratamientos', component: Tratamientos }
 ];
