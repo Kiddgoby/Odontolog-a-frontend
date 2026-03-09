518b750cec4263478d3081c35dd85ed67d94ea2e
Javier G. <xavichavochavuski@gmail.com>
2026-03-09 16:26:04 +0100

    Change in rutes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,6 @@ import { Calendar } from './calendar/calendar';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-<<<<<<< HEAD
     { path: 'login', component: LoginComponent, data: { title: 'Acceso' } },
     { path: 'home', component: HomeComponent, data: { title: 'Panel de Control' } },
     { path: 'patient', component: Patient, data: { title: 'Listado de Pacientes' } },
@@ -19,15 +18,5 @@ export const routes: Routes = [
     { path: 'patient/:id', component: PacienteDetail, data: { title: 'Detalle de Paciente' } },
     { path: 'tratamientos', component: Tratamientos, data: { title: 'Gestión de Tratamientos' } },
     { path: 'citas', component: Appointment, data: { title: 'Citas Médicas' } },
-    { path: 'calendario', component: Calendario, data: { title: 'Calendario de Citas' } }
-=======
-    { path: 'login', component: LoginComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'patient', component: Patient },
-    { path: 'patient/new', component: NewPatient },
-    { path: 'patient/:id', component: PacienteDetail },
-    { path: 'tratamientos', component: Tratamientos },
-    { path: 'citas', component: Appointment },
-    { path: 'calendar', component: Calendar }
->>>>>>> fd71c576082ab4b1fb0aa4e7908b4ff1e3689619
+    { path: 'calendar', component: Calendar, data: { title: 'Calendario de Citas' } }
 ];
